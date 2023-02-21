cfba89cd34e51c862c5b06f7a65431c1a8e7b671
harshrajat <harsh@epns.io>
2023-02-22 01:25:13 +0400

    Added full Push Notification SDK coverage

--- sdk-functionality/index.js
+++ sdk-functionality/index.js
@@ -14,50 +14,68 @@ console.log("\n");
 console.log(chalk.bgBlue.white.bold("SDK FUNCTIONALITIES"));
 
 // setting wallet params
-const walletAddress = "0xD8634C39BBFd4033c0d3289C4515275102423681";
 const walletPrivateKey = process.env.WALLET_PRIVATE_KEY;
 const channelAddress = "0x74415Bc4C4Bf4Baecc2DD372426F0a1D016Fa924"; // can be your wallet address as well if you own a channel
-const _env = 'staging'; // can be 'staging', 'prod' or 'dev' | Other values might result in incorrect responses
+
+// can be 'staging', 'prod' or 'dev' | Other values might result in incorrect responses
+// if taking these values for prod, ensure you change CAIP-10 format from eip155:5:0xAddress (GOERLI) to eip155:1:0xAddress (MAINNET) address in parameters passed
+// ensure provider matches the network id as well
+const _env = 'staging'; 
 
 // initialize signer for whatever function it's needed for
-const provider = ethers.getDefaultProvider();
+const provider = ethers.getDefaultProvider(5);
 
 const Pkey = `0x${walletPrivateKey}`;
-var wallet = new ethers.Wallet(Pkey, provider);
-const _signer = new ethers.Wallet(Pkey);
+const _signer = new ethers.Wallet(Pkey, provider);
+const walletAddress = _signer.address;
+
+// generate some dummy wallets as well
+const walletAddressAlt2 = "0xCdBE6D076e05c5875D90fa35cc85694E1EAFBBd1";
 
 // test dotenv and wallet
 console.log(chalk.gray("Testing ENV file, should display wallet private key - " + process.env.WALLET_PRIVATE_KEY));
-console.log(chalk.gray("Testing Wallet addres from private key - " + wallet.address));
+console.log(chalk.gray("Testing Wallet addres from private key - " + walletAddress));
 
-// Run Notifications Use cases
+// Push Notification - Run Notifications Use cases
 async function runNotificaitonsUseCases() {
-  // console.log(chalk.bgGreen.bold("PushAPI.user.getFeeds"));
-  // await PushAPI_user_getFeeds();
+  console.log(chalk.bgGreen.bold("PushAPI.user.getFeeds"));
+  await PushAPI_user_getFeeds();
 
-  // console.log(chalk.bgGreen.bold("PushAPI.user.getFeeds [Spam]"));
-  // await PushAPI_user_getFeeds__spam();
+  console.log(chalk.bgGreen.bold("PushAPI.user.getFeeds [Spam]"));
+  await PushAPI_user_getFeeds__spam();
 
-  // console.log(chalk.bgGreen.bold("PushAPI.user.getSubscriptions"));
-  // await PushAPI_user_getSubscriptions();
+  console.log(chalk.bgGreen.bold("PushAPI.user.getSubscriptions"));
+  await PushAPI_user_getSubscriptions();
 
-  // console.log(chalk.bgGreen.bold("PushAPI.channels.getChannel()"));
-  // await PushAPI_channels_getChannel();
+  console.log(chalk.bgGreen.bold("PushAPI.channels.getChannel()"));
+  await PushAPI_channels_getChannel();
 
-  // console.log(chalk.bgGreen.bold("PushAPI.channels.search()"));
-  // await PushAPI_channels_search();
+  console.log(chalk.bgGreen.bold("PushAPI.channels.search()"));
+  await PushAPI_channels_search();
 
-  // console.log(chalk.bgGreen.bold("PushAPI.channels.subscribe()"));
-  // await PushAPI_channels_subscribe();
+  console.log(chalk.bgGreen.bold("PushAPI.channels.subscribe()"));
+  await PushAPI_channels_subscribe();
 
-  // console.log(chalk.bgGreen.bold("PushAPI.channels.unsubscribe()"));
-  // await PushAPI_channels_unsubscribe();
+  console.log(chalk.bgGreen.bold("PushAPI.channels.unsubscribe()"));
+  await PushAPI_channels_unsubscribe();
 
-  console.log(chalk.bgGreen.bold("PushAPI.payloads.sendNotification()"));
+  console.log(chalk.bgGreen.bold("PushAPI.payloads.sendNotification() [Direct Payload, Single Recipient]"));
   await PushAPI_payloads_sendNotification__direct_payload_single_recipient();
+
+  console.log(chalk.bgGreen.bold("PushAPI.payloads.sendNotification() [Direct Payload, Batch of Recipients (Subset)]"));
+  await PushAPI_payloads_sendNotification__direct_payload_group_of_recipient_subset();
+
+  console.log(chalk.bgGreen.bold("PushAPI.payloads.sendNotification() [Direct Payload, All Recipients (Broadcast)]"));
+  await PushAPI_payloads_sendNotification__direct_payload_all_recipients_brodcast();
+
+  // IMPORTANT: VARIOUS OTHER NOTIFICATIONS FORMAT SUPPORTED
+  // EXAMPLES HERE: https://github.com/ethereum-push-notification-service/push-sdk/blob/main/packages/restapi/README.md
+
+  console.log(chalk.bgGreen.bold("PushAPI.channels._getSubscribers()"));
+  await PushAPI_channels_getSubscribers();
 }
 
-// PushAPI.user.getFeeds
+// Push Notification - PushAPI.user.getFeeds
 async function PushAPI_user_getFeeds() {
   const notifications = await PushAPI.user.getFeeds({
     user: `eip155:5:${walletAddress}`, // user address in CAIP
@@ -68,7 +86,7 @@ async function PushAPI_user_getFeeds() {
   console.log(notifications);
 }
 
-// PushAPI.user.getFeeds - Spam
+// Push Notification - PushAPI.user.getFeeds - Spam
 async function PushAPI_user_getFeeds__spam() {
   const notifications = await PushAPI.user.getFeeds({
     user: `eip155:5:${walletAddress}`, // user address in CAIP
@@ -80,7 +98,7 @@ async function PushAPI_user_getFeeds__spam() {
   console.log(notifications);
 }
 
-// PushAPI.user.getSubscriptions
+// Push Notification - PushAPI.user.getSubscriptions
 async function PushAPI_user_getSubscriptions() {
   const subscriptions = await PushAPI.user.getSubscriptions({
     user: `eip155:5:${walletAddress}`, // user address in CAIP
@@ -91,7 +109,7 @@ async function PushAPI_user_getSubscriptions() {
   console.log(subscriptions);
 }
 
-// PushAPI.channels.getChannel
+// Push Notification - PushAPI.channels.getChannel
 async function PushAPI_channels_getChannel() {
   const channelData = await PushAPI.channels.getChannel({
     channel: `eip155:5:${walletAddress}`, // user address in CAIP
@@ -102,7 +120,7 @@ async function PushAPI_channels_getChannel() {
   console.log(channelData);
 }
 
-// PushAPI.channels.search
+// Push Notification - PushAPI.channels.search
 async function PushAPI_channels_search() {
   const channelsData = await PushAPI.channels.search({
     query: 'push', // a search query
@@ -115,7 +133,7 @@ async function PushAPI_channels_search() {
   console.log(channelsData);
 }
 
-// PushAPI.channels.subscribe
+// Push Notification - PushAPI.channels.subscribe
 async function PushAPI_channels_subscribe() {
   const response = await PushAPI.channels.subscribe({
     signer: _signer,
@@ -134,7 +152,7 @@ async function PushAPI_channels_subscribe() {
   console.log(response);
 }
 
-// PushAPI.channels.unsubscribe
+// Push Notification - PushAPI.channels.unsubscribe
 async function PushAPI_channels_unsubscribe() {
   const response = await PushAPI.channels.unsubscribe({
     signer: _signer,
@@ -153,12 +171,12 @@ async function PushAPI_channels_unsubscribe() {
   console.log(response);
 }
 
-// Send Notifications
+// Push Notification - Send Notifications
 // // Direct payload for single recipient(target)
 // // // PushAPI.payloads.sendNotification
 async function PushAPI_payloads_sendNotification__direct_payload_single_recipient() {
   const apiResponse = await PushAPI.payloads.sendNotification({
-    _signer, // Needs to resolve to channel address
+    signer: _signer, // Need to resolve to channel address
     type: 3, // target
     identityType: 2, // direct payload
     notification: {
@@ -180,10 +198,92 @@ async function PushAPI_payloads_sendNotification__direct_payload_single_recipien
   console.log(apiResponse);
 }
 
+// // Push Notification - Direct payload for group of recipients(subset)
+// // // PushAPI.payloads.sendNotification
+async function PushAPI_payloads_sendNotification__direct_payload_group_of_recipient_subset() {
+  const apiResponse = await PushAPI.payloads.sendNotification({
+    signer: _signer, // Need to resolve to channel address
+    type: 4, // subset
+    identityType: 2, // direct payload
+    notification: {
+      title: `[SDK-TEST] notification TITLE:`,
+      body: `[sdk-test] notification BODY`
+    },
+    payload: {
+      title: `[sdk-test] payload title`,
+      body: `sample msg body`,
+      cta: '',
+      img: ''
+    },
+    recipients: [`eip155:5:${walletAddress}`, `eip155:5:${walletAddressAlt2}`], // recipient addresses
+    channel: `eip155:5:${walletAddress}`, // your channel address
+    env: _env
+  });
+
+  console.log(chalk.gray("PushAPI.payloads.sendNotification | Response - 204 OK"));
+  console.log(apiResponse);
+}
+
+// // Push Notification - Direct payload for all recipients(broadcast)
+// // // PushAPI.payloads.sendNotification
+async function PushAPI_payloads_sendNotification__direct_payload_all_recipients_brodcast() {
+  const apiResponse = await PushAPI.payloads.sendNotification({
+    signer: _signer, // Needs to resolve to channel address
+    type: 1, // broadcast
+    identityType: 2, // direct payload
+    notification: {
+      title: `[SDK-TEST] notification TITLE:`,
+      body: `[sdk-test] notification BODY`
+    },
+    payload: {
+      title: `[sdk-test] payload title`,
+      body: `sample msg body`,
+      cta: '',
+      img: ''
+    },
+    channel: `eip155:5:${walletAddress}`, // your channel address
+    env: _env
+  });
+
+  console.log(chalk.gray("PushAPI.payloads.sendNotification | Response - 204 OK"));
+  console.log(apiResponse);
+}
+
+// Push Notification - Get Subscribers list from channels (DEPRECATED)
+async function PushAPI_channels_getSubscribers() {
+  const subscribers = await PushAPI.channels._getSubscribers({
+    channel: `eip155:5:${channelAddress}`, // channel address in CAIP
+    env: 'staging'
+  });
+
+  console.log(chalk.gray("PushAPI.channels._getSubscribers | Response - 200 OK"));
+  console.log(subscribers);
+}
+
+// Push Chat - Run Chat Use cases
+async function runChatUseCases() {
+  console.log(chalk.bgGreen.bold("PushAPI_user_create"));
+  await PushAPI_user_create();
+}
+
+// Push Chat - Create User
+async function PushAPI_user_create() {
+  const user = await PushAPI.user.create({
+    account: walletAddress,
+    env: 'staging',
+  });
+
+  console.log(chalk.gray("PushAPI_user_create | Response - 200 OK"));
+  console.log(user);
+}
 
 // Master control
+// -----
 console.log(chalk.bgYellow("All features of Push Notifications"));
 await runNotificaitonsUseCases();
+
+console.log(chalk.bgYellow("\nAll features of Push Chat"));
+await runChatUseCases();
 
 console.log(chalk.bgBlue.white.bold("SDK FUNCTIONALITIES END"));
 console.log("\n");
